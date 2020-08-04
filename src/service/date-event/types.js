92f1bc493484bba4f97e75f092c00e54d26c7562
Kharis Khasburrahman <khasburrahman@yahoo.com>
2020-08-05 00:28:58 +0700

    chore: add date event type

---
+++ src/service/date-event/types.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} DateEvent
+ * @property {String} d - date
+ * @property {String} m - month
+ * @property {String} y - year
+ * @property {String} event - event that is in the specified date
+ * @property {String} category - category of the event
+ */
